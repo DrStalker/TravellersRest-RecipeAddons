9f9f734ac1cc2c957ca139cbbf664d7695701020
Doug <doug.stalker@gmail.com>
2024-08-07 11:14:48 +1000

    initial project files

---
+++ Install.bat
@@ -0,0 +1,1 @@
+copy "D:\TR\RecipeAddons\bin\Debug\netstandard2.1\net.nep.bepinex.recipeaddons.dll"  "C:\Games\Steam\steamapps\common\Travellers Rest\Windows\BepInEx\plugins\"
---
+++ RecipeAddons.cs
@@ -0,0 +1,48 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+
+namespace RecipeAddons
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private static Harmony _harmony;
+        internal static ManualLogSource Log;
+
+        private static ConfigEntry<bool> _debugLogging;
+
+
+        public Plugin()
+        {
+            // bind to config settings
+            _debugLogging = Config.Bind("Debug", "Debug Logging", false, "Logs additional information to console");
+
+        }
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Log = Logger;
+            _harmony = Harmony.CreateAndPatchAll(typeof(Plugin));
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        private void OnDestroy()
+        {
+            _harmony.UnpatchSelf();
+        }
+        public static void DebugLog(string message)
+        {
+            // Log a message to console only if debug is enabled in console
+            if (_debugLogging.Value)
+            {
+                Log.LogInfo(string.Format("NepEasyFishing: Debug: {0}", message));
+            }
+        }
+
+
+    }
+}
