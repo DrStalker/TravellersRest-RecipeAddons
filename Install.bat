8d82414035f737ac918b3fb901ffdafd7f36a2f8
Doug <doug.stalker@gmail.com>
2024-08-26 11:01:38 +1000

    compiled release

--- Install.bat
+++ Install.bat
@@ -1,1 +1,1 @@
-copy "D:\TR\RecipeAddons\bin\Debug\netstandard2.1\net.nep.bepinex.recipeaddons.dll"  "C:\Games\Steam\steamapps\common\Travellers Rest\Windows\BepInEx\plugins\"
+copy "D:\TR\RecipeAddons\bin\Debug\netstandard2.1\net.nep.bepinex.creativecooking.dll"  "C:\Games\Steam\steamapps\common\Travellers Rest\Windows\BepInEx\plugins\"
